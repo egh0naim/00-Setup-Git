995837df37bfb26adff368cce861f12d49ec6d6a
egh0naim <estabraqghonaim@gmail.com>
2026-01-29 16:13:58 +0300

    Implement my_name_is function and fix style issues

--- today.py
+++ today.py
@@ -1,9 +1,14 @@
-# pylint: disable=C0116,C0303
+"""Simple functions returning my name and age."""
 
+# pylint: disable=C0116,C0303
 
 # $CHALLENGIFY_BEGIN
 def my_name_is():
- 
-    pass
+    """Return my GitHub nickname."""
+    return "egh0naim"
 
+
+def my_age_is():
+    """Return my age."""
+    return "28"
 # $CHALLENGIFY_END
